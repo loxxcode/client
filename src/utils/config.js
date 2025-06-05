3f7b1c091f66d70e4cd57f902bf5b24bef794a6e
loxxcode <loxxhypolite@gmail.com>
2025-06-05 23:02:29 +0200

    update BASE_URL to use absolute URL for API calls

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 // src/config.js
 // Use a relative URL to leverage Vercel's proxy and avoid CORS issues
-export const BASE_URL = "/api/auth/login";
+export const BASE_URL = "https://server-az7z.onrender.com";
